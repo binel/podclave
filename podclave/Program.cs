9ddc68ebd8241bd88e5ffaddb7191fe2be346f68
Ben Nelson <binelson13@gmail.com>
2024-03-02 14:33:09 -0600

    Initial Commit
    I want to create a replacement for my current python script for
    downloading and archiving podcasts. The limitations of the current
    script are that it's brittle, it can't handle all RSS feeds, and it's
    logs leave a lot to be desired. I'm hoping a C# rewrite will can resolve
    these issues. I am choosing C# so this can also be a practice project
    for my day job.

---
+++ podclave/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Podclaves
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
