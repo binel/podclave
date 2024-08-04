920c3ce875d7d4ad2210e0721eda6335a06511a8
Ben Nelson <binelson13@gmail.com>
2024-08-04 11:21:51 -0500

    Updating publish to produce a single executable

--- publish.sh
+++ publish.sh
@@ -1,1 +1,1 @@
-dotnet publish podclave.cli/podclave.cli.csproj -c Release -o $1 -r linux-x64
+dotnet publish podclave.cli/podclave.cli.csproj -c Release -o $1 -r linux-x64 /p:PublishSingleFile=true --self-contained
